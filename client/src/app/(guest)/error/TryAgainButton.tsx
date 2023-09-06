0cd473c5972b6971fbc9b481d6a07e15134d76c3
Marcin Skic <marcinskicoficjal@gmail.com>
2023-09-06 21:32:11 +0200

    Make error page try to reconnect every half a minute

--- client/src/app/(guest)/error/TryAgainButton.tsx
+++ client/src/app/(guest)/error/TryAgainButton.tsx
@@ -1,15 +1,25 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function TryAgainButton() {
     const router = useRouter();
     const [attempts, setAttempts] = useState(0);
+    const reconectInterval = useRef<NodeJS.Timeout | null>(null);
 
     useEffect(() => {
         router.push("/");
         router.refresh();
+
+        reconectInterval.current = setInterval(() => {
+            router.push("/");
+            router.refresh();
+        }, 30 * 1000);
+
+        return () => {
+            clearInterval(reconectInterval.current!!);
+        };
     }, [router]);
 
     return (
