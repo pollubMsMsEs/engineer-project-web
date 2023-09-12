bd85e79a35608ae478ff521db3f05d60d4f99abb
Marcin Skic <marcinskicoficjal@gmail.com>
2023-09-12 19:32:26 +0200

    Migrate person create page

---
+++ client/src/app/(user)/person/create/page.tsx
@@ -0,0 +1,6 @@
+import PersonForm from "@/components/PersonForm";
+import React from "react";
+
+export default function PersonCreate() {
+    return <PersonForm />;
+}
---
+++ client/src/components/PersonForm.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Person, PersonFromAPI } from "../types/movieType";
+import ErrorsDisplay from "../components/ErrorsDisplay";
+import { useRouter } from "next/navigation";
+
+export default function PersonForm({ person }: { person?: PersonFromAPI }) {
+    const router = useRouter();
+
+    const [name, setName] = useState(person?.name ?? "");
+    const [nick, setNick] = useState(person?.nick ?? "");
+    const [surname, setSurname] = useState(person?.surname ?? "");
+
+    const [errors, setErrors] = useState([]);
+
+    async function submitForm() {
+        const submittedPerson: Person & { _id: string | undefined } = {
+            _id: person?._id,
+            name,
+            nick: nick === "" ? undefined : nick,
+            surname,
+        };
+
+        console.log(submittedPerson);
+
+        const response = person
+            ? await fetch(`/api/person/${person._id}`, {
+                  method: "PUT",
+                  headers: {
+                      "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(submittedPerson),
+              })
+            : await fetch(`/api/person/create`, {
+                  method: "POST",
+                  headers: {
+                      "Content-Type": "application/json",
+                  },
+                  body: JSON.stringify(submittedPerson),
+              });
+        console.log(response);
+        const result = await response.json();
+
+        if (result.errors) {
+            setErrors(result.errors);
+        } else {
+            router.push("/person/all");
+        }
+    }
+
+    return (
+        <div
+            style={{
+                display: "grid",
+                gridTemplateColumns: "1fr max(200px,10%)",
+            }}
+        >
+            <form
+                style={{ display: "grid", justifyContent: "start" }}
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    e.currentTarget.reportValidity();
+                    submitForm();
+                }}
+            >
+                {person?._id && (
+                    <input type="hidden" name="_id" value={person?._id} />
+                )}
+                <label htmlFor="name">Name: </label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                    required
+                    onChange={(e) => {
+                        setName(e.target.value);
+                    }}
+                />
+                <label htmlFor="nick">Nick: </label>
+                <input
+                    type="text"
+                    name="nick"
+                    id="nick"
+                    value={nick}
+                    onChange={(e) => {
+                        setNick(e.target.value);
+                    }}
+                />
+                <label htmlFor="surname">Surname: </label>
+                <input
+                    type="text"
+                    name="surname"
+                    id="surname"
+                    required
+                    value={surname}
+                    onChange={(e) => {
+                        setSurname(e.target.value);
+                    }}
+                />
+                <button type="submit">{person?._id ? "Update" : "Add"}</button>
+            </form>
+            <ErrorsDisplay errors={errors} />
+        </div>
+    );
+}
