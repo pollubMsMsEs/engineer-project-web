a5f7599dcd72ff57e3a52d9c6d4e6e5b8efb13c1
Marcin Skic <marcinskicoficjal@gmail.com>
2023-09-13 12:15:58 +0200

    Migrate person edit page. Finish migration

---
+++ client/src/app/(user)/person/[_id]/edit/page.tsx
@@ -0,0 +1,17 @@
+import PersonForm from "@/components/PersonForm";
+import { fetchAPIFromServerComponent } from "@/modules/serverSide";
+import React from "react";
+
+export default async function PersonEdit({
+    params,
+}: {
+    params: { _id: string };
+}) {
+    const response = await fetchAPIFromServerComponent(
+        `/person/${params._id}`,
+        0
+    );
+    const person = (await response.json()).data;
+
+    return <PersonForm person={person} />;
+}
