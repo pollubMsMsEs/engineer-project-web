5a74e7f6f3717fd7ba2c30831873939452f2ca20
Marcin Skic <marcinskicoficjal@gmail.com>
2023-09-26 21:18:07 +0200

    Allow images from localhost & render API

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "engineer-project-api.onrender.com",
+                port: "",
+                pathname: "**",
+            },
+            {
+                protocol: "http",
+                hostname: "localhost",
+                port: "7777",
+                pathname: "**",
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
