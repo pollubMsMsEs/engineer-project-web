251edde2024a2d0754101d6ac65ebc4f5e3ff777
Marcin Skic <marcinskicoficjal@gmail.com>
2023-09-28 15:50:20 +0200

    Add to work schema normalizer of URL
    This solution is very not idiomatic aka. shouldn't be used BUT it
    works without breaking anything and now URL of cover returned by server
    will always work no matter if localhost or cloud hosting.

--- server/models/work.ts
+++ server/models/work.ts
@@ -1,33 +1,63 @@
 import { Schema, model } from "mongoose";
 
-const WorkSchema = new Schema({
-    created_by: { type: Schema.Types.ObjectId, ref: "User" },
-    title: { type: String, required: true },
-    cover: { type: String },
-    dev: { type: Boolean, default: true },
-    description: String,
-    published_at: Date,
-    genres: [String], //TODO: Should enforce uniqueness of values in array?
-    metadata: {
-        type: Map,
-        of: [String],
-    },
-    people: [
-        {
-            _id: false, //Mongoose by default creates id for subdocuments, this stops it from doing that
-            person_id: {
-                type: Schema.Types.ObjectId,
-                ref: "Person",
-                required: true,
+function normalizeCoverURL(work: any & { cover?: string }) {
+    let baseUrl;
+    if (!process.env.PRODUCTION) {
+        baseUrl = `http://localhost:${process.env.port}/api/image/`;
+    } else {
+        baseUrl = `${process.env.URL}/api/image/`;
+    }
+
+    if (!work?.cover || work.cover.startsWith(baseUrl)) {
+        console.log("not transformed");
+        return work;
+    } else {
+        return { ...work, cover: baseUrl + work.cover.split("/").pop() };
+    }
+}
+
+const WorkSchema = new Schema(
+    {
+        created_by: { type: Schema.Types.ObjectId, ref: "User" },
+        title: { type: String, required: true },
+        cover: { type: String },
+        dev: { type: Boolean, default: true },
+        description: String,
+        published_at: Date,
+        genres: [String], //TODO: Should enforce uniqueness of values in array?
+        metadata: {
+            type: Map,
+            of: [String],
+        },
+        people: [
+            {
+                _id: false, //Mongoose by default creates id for subdocuments, this stops it from doing that
+                person_id: {
+                    type: Schema.Types.ObjectId,
+                    ref: "Person",
+                    required: true,
+                },
+                role: { type: String, required: true },
+                details: {
+                    type: Map,
+                    of: [String],
+                },
             },
-            role: { type: String, required: true },
-            details: {
-                type: Map,
-                of: [String],
+        ],
+        type: { type: String, enum: ["movie", "book", "computerGame"] },
+    },
+    {
+        toObject: {
+            transform: function (doc, ret, options) {
+                return normalizeCoverURL(ret);
             },
         },
-    ],
-    type: { type: String, enum: ['movie', 'book', 'computerGame'] },
-});
+        toJSON: {
+            transform: function (doc, ret, options) {
+                return normalizeCoverURL(ret);
+            },
+        },
+    }
+);
 
 export default model("Work", WorkSchema, "works");
