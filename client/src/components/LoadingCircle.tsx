588ac36864d978b12b365f3e475717e3e7de85e2
Marcin Skic <marcinskicoficjal@gmail.com>
2023-10-09 19:51:13 +0200

    Add option to add text to loading circle

--- client/src/components/LoadingCircle.tsx
+++ client/src/components/LoadingCircle.tsx
@@ -1,15 +1,29 @@
 import styles from "./loadingCircle.module.scss";
 
-export default function LoadingCircle({ size }: { size: string }) {
+export default function LoadingCircle({
+    size,
+    text,
+}: {
+    size: string;
+    text?: string;
+}) {
     return (
         <div
+            className={styles["loading-circle"]}
             style={{
-                width: size,
                 height: size,
-                border: `calc(${size} * 0.3) solid #f3f3f3`,
-                borderTop: `calc(${size} * 0.3) solid #3498db`,
             }}
-            className={styles["component-circle-loader"]}
-        ></div>
+        >
+            <div
+                className={styles["loading-circle__circle"]}
+                style={{
+                    aspectRatio: "1 / 1",
+                    height: "90%",
+                    border: `calc(${size} * 0.3) solid #f3f3f3`,
+                    borderTop: `calc(${size} * 0.3) solid #3498db`,
+                }}
+            ></div>
+            {text && <span>{text}</span>}
+        </div>
     );
 }
