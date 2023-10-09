04ace7829b61179ec99beb309b90c7d80308adf2
MarcinSkrzypek <73038475+MarcinSkrzypek@users.noreply.github.com>
2023-10-09 22:18:16 +0200

    Change endpoint workInstance/currentUser to workInstance/me

--- server/routes/api/workInstance.ts
+++ server/routes/api/workInstance.ts
@@ -13,7 +13,7 @@ const router = Router();
 
 router.get("/all", getAll);
 router.get("/all/:id", getAllForUser);
-router.get("/currentUser", getAllForCurrentUser);
+router.get("/me", getAllForCurrentUser);
 router.post("/create", createOne);
 router.get("/:id", getOne);
 router.put("/:id", updateOne);
