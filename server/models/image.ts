dee5935b4bce51e1f0afc934da3845c09ef6d411
MarcinSkrzypek <73038475+MarcinSkrzypek@users.noreply.github.com>
2023-10-09 22:51:49 +0200

    Add storing types for images

--- server/models/image.ts
+++ server/models/image.ts
@@ -2,6 +2,20 @@ import { Schema, model } from "mongoose";
 
 const ImageSchema = new Schema({
     image: { type: String },
+    type: {
+        type: String,
+        enum: [
+            "image/apng",
+            "image/avif",
+            "image/gif",
+            "image/jpeg",
+            "image/png",
+            "image/svg+xml",
+            "image/webp",
+            "application/xml",
+        ],
+        required: true,
+    },
 });
 
 export default model("Image", ImageSchema, "images");
