0b875296280e2516bb021b8d3c21da2b2d38a8b9
MarcinSkic <marcinskicoficjal@gmail.com>
2023-10-23 18:48:34 +0200

    Begin creating workInstance

--- client/src/app/(user)/search/page.tsx
+++ client/src/app/(user)/search/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React, { useRef, useState } from "react";
 import styles from "./page.module.scss";
 import Icon from "@mdi/react";
@@ -12,6 +12,8 @@ import InstancesGrid from "@/components/InstancesGrid";
 import WorkCard from "@/components/workCard/WorkCard";
 import ClickableCard from "@/components/clickableCard/ClickableCard";
 import { waitPromise, waitRandomPromise } from "@/scripts/devUtils";
+import { toast } from "react-toastify";
+import { DEFAULT_WORK_INSTANCE } from "@/constantValues";
 
 function assertCorrectType(type: any, defaultType: WorkType = "book") {
     if (type === "book" || type === "movie" || type === "game") {
@@ -22,6 +24,7 @@ function assertCorrectType(type: any, defaultType: WorkType = "book") {
 }
 
 export default function Search() {
+    const router = useRouter();
     const searchParams = useSearchParams();
     const [type, setType] = useState<WorkType>(() => {
         return assertCorrectType(searchParams.get("type"));
@@ -44,6 +47,83 @@ export default function Search() {
             setFoundWorks(await searchWorks(query, type));
             setIsFetching(false);
         }, 1000);
+    }
+
+    async function createWorkInstance(work: WorkFromAPIShort) {
+        const newWorkFromAPI = {
+            api_id: work.api_id,
+            type: work.type,
+        };
+
+        const responseWorkFromAPI = await fetch("/api/workFromAPI/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newWorkFromAPI),
+        });
+
+        if (!responseWorkFromAPI.ok) {
+            try {
+                const workFromAPI = await responseWorkFromAPI.json();
+
+                if (workFromAPI.errors) {
+                    toast.error(workFromAPI.errors[0].msg);
+                } else if (workFromAPI.message) {
+                    toast.error(workFromAPI.message);
+                } else if (workFromAPI.error) {
+                    toast.error(workFromAPI.error);
+                }
+            } catch (e) {
+                toast.error("Unknown error");
+                console.error(e);
+            }
+
+            return false;
+        }
+
+        const workFromAPI = (await responseWorkFromAPI.json()).created;
+
+        const newWorkInstance = {
+            ...DEFAULT_WORK_INSTANCE,
+            work_id: workFromAPI._id,
+            onModel: "WorkFromAPI",
+        };
+
+        const responseInstance = await fetch("/api/workInstance/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newWorkInstance),
+        });
+
+        if (!responseInstance.ok) {
+            try {
+                const workInstance = await responseInstance.json();
+
+                if (workInstance.errors) {
+                    toast.error(workInstance.errors[0].msg);
+                } else if (workInstance.message) {
+                    toast.error(workInstance.message);
+                } else if (workInstance.error) {
+                    toast.error(workInstance.error);
+                }
+            } catch (e) {
+                toast.error("Unknown error");
+                console.error(e);
+            }
+
+            return false;
+        }
+
+        const workInstance = await responseInstance.json();
+
+        if (workInstance.acknowledged) {
+            router.push(`/me/work/${workInstance.created._id}`);
+        }
+
+        return true;
     }
 
     return (
@@ -96,10 +176,12 @@ export default function Search() {
                                           <ClickableCard
                                               key={work.api_id}
                                               onClick={async () => {
-                                                  await waitRandomPromise(2000);
                                                   return {
-                                                      success: true,
-                                                      stopLoading: true,
+                                                      success:
+                                                          await createWorkInstance(
+                                                              work
+                                                          ),
+                                                      stopLoading: false,
                                                   };
                                               }}
                                           >
