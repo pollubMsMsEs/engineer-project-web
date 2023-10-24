597eee59c685b05fa8a1305af67c717fe228b06d
Marcin Skic <marcinskicoficjal@gmail.com>
2023-10-24 20:28:29 +0200

    Remove unused pages

--- client/src/components/Person.tsx
+++ client/src/components/Person.tsx
@@ -1,13 +1,43 @@
-import { MetaObject, Person } from "../types/types";
+"use client";
+import { useRouter } from "next/navigation";
+import { MetaObject, Person, PersonFromAPI } from "../types/types";
 import styles from "./person.module.scss";
+import Icon from "@mdi/react";
+import { mdiTrashCan } from "@mdi/js";
+import { toast } from "react-toastify";
+
+async function deletePerson(_id: string) {
+    const response = await fetch(`/api/person/${_id}`, {
+        method: "DELETE",
+    });
+    const result = await response.json();
+
+    const msg = result?.errors?.[0]?.msg;
+    if (msg) {
+        toast.error(msg);
+        return false;
+    }
+
+    return result;
+}
 
 export default function Person({
     person,
     details,
 }: {
-    person: Person;
+    person: PersonFromAPI;
     details?: MetaObject;
 }) {
+    const router = useRouter();
+
+    async function deletePersonHandler(_id: string) {
+        const result = await deletePerson(_id);
+        const deletedPersonId = result && result.deleted?._id;
+        if (deletedPersonId) {
+            router.push("/");
+        }
+    }
+
     return (
         <>
             <div>{person.name}</div>
@@ -26,6 +56,13 @@ export default function Person({
                     ))}
                 </div>
             )}
+            <button
+                onClick={() => {
+                    deletePersonHandler(person._id);
+                }}
+            >
+                <Icon path={mdiTrashCan} size={1} />
+            </button>
         </>
     );
 }
