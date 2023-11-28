46d9c4ce950be5d1af8c5cf6c41844110172f966
Marcin Skic <marcinskicoficjal@gmail.com>
2023-11-28 22:10:16 +0100

    Add custom aspect ratios to workCards based on type
    Depending on work type, covers have typicaly different ratios:
    Books: 1/1.575
    Movies: 2/3
    Games: 3/4
    
    Also changed object-fit to fill, slight distortion in case of books
    is worth not seeing resizing after page loads

--- client/src/components/workCard/WorkCard.tsx
+++ client/src/components/workCard/WorkCard.tsx
@@ -2,20 +2,36 @@ import Image from "next/image";
 import React from "react";
 import ImageContainer from "../imageContainer/ImageContainer";
 import styles from "./workCard.module.scss";
+import { WorkType } from "@/types/types";
 
 export default function WorkCard({
     work,
     roundedCornersTop = false,
 }: {
-    work: { title?: string; cover?: string };
+    work: { title?: string; cover?: string; type: WorkType };
     roundedCornersTop?: boolean;
 }) {
+    let aspectRatio;
+
+    switch (work.type) {
+        case "book":
+            aspectRatio = "1/1.575";
+            break;
+        case "movie":
+            aspectRatio = "2/3";
+            break;
+        case "game":
+            aspectRatio = "3/4";
+            break;
+    }
+
     return (
         <div className={styles["work-card"]}>
             <ImageContainer
-                cover={work.cover}
+                src={work.cover}
                 alt="Work cover"
                 roundedCornersTop
+                aspectRatio={aspectRatio}
             />
             <div className={styles["work-card__title"]}>{work.title}</div>
         </div>
