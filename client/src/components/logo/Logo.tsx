bdabd730d4364ec8139894f713935a1f837a1087
Marcin Skic <marcinskicoficjal@gmail.com>
2023-12-16 13:37:07 +0100

    Update app logo and name

--- client/src/components/logo/Logo.tsx
+++ client/src/components/logo/Logo.tsx
@@ -1,12 +1,19 @@
 import Icon from "@mdi/react";
 import { mdiMovieOpen } from "@mdi/js";
 import styles from "./logo.module.scss";
+import Image from "next/image";
+import icon from "@/../public/icon.png";
 
 export default function Logo() {
     return (
         <h1 className={styles["logo"]}>
-            <span>Popculture Database</span>
-            <Icon className={styles["logo__icon"]} path={mdiMovieOpen} />
+            <span>DigiShelf</span>
+            <Image
+                className={styles["logo__icon"]}
+                src={icon}
+                height={80}
+                alt="icon"
+            />
         </h1>
     );
 }
