ac210c5fb3a727baad826bb2303f6687e517b84f
Marcin Skic <marcinskicoficjal@gmail.com>
2023-12-17 12:38:38 +0100

    Fix status label

--- client/src/components/workCollection/WorkCollection.tsx
+++ client/src/components/workCollection/WorkCollection.tsx
@@ -57,7 +57,7 @@ export default function WorkCollection({
                 <Select
                     id="typeQuery"
                     name="typeQuery"
-                    label="Type"
+                    label="Status"
                     labelDisplay="always"
                     value={statusQuery}
                     options={[
